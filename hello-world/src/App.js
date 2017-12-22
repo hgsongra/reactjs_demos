764d1dcaeba8043da586029557eaa1ca83ff3e9f
Hitesh Songra <hgsongra@ymail.com>
2017-12-22 12:49:56 +0530

    Add Hello World code

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <h1>Hello World!</h1>
+        <h2>Welcome to ReactJS</h2>
+        <p>This is Lesson#001</p>
       </div>
     );
   }
